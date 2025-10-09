c3ea5f8578fc2ad923e871915ebcd1b5b91f401a
Alexander <floresjairo25@gmail.com>
2025-10-09 14:27:01 -0600

    feat(auth): módulo Auth con JWT + Swagger; PrismaModule estable

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,29 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Validación global para DTOs
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  // Validación global de DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,   // elimina propiedades no declaradas en DTOs
+      transform: true,   // convierte tipos (string->number, etc.)
+    }),
+  );
 
-  // Config básica de Swagger
+  // (Opcional) habilitar CORS si lo necesitas
+  // app.enableCors();
+
+  // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('My First Project – API')
     .setDescription('NestJS + Prisma demo')
     .setVersion('1.0.0')
-    .addTag('users') // etiqueta para el módulo Users
+    .addTag('users')
+    .addTag('auth')
+    .addBearerAuth() // <-- agrega soporte para Authorization: Bearer <token>
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // UI en /api
+  SwaggerModule.setup('api', app, document); // UI en http://localhost:3000/api
 
   await app.listen(3000);
 }
